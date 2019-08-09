f41946cd2f832ef78c98bd98b8e0cb82fd9329d2
Smallflyfly <452639095@QQ.COM>
2019-08-09 16:00:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  gameName="星际争霸2";
+
+  render(){
+    return (
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          游戏名：{this.gameName}
+        </div>
+        <div className="panel-body">
+          这是区域的主体
+        </div>
+        <div className="panel-footer">
+          <button className="btn btn-success">确定</button>
+          <button className="btn btn-danger">删除</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
